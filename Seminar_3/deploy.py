528a73df7c003a7cc0285c462e56e55b9cafb2c0
ustinowa <a.s.ustinowa@gmail.com>
2023-07-31 22:23:53 +0300

    Update Seminar_3

---
+++ Seminar_3/deploy.py
@@ -0,0 +1,11 @@
+from  sshcheckers import ssh_checkout, upload_files
+
+def deploy():
+    res = []
+    upload_files("0.0.0.0", "user2", "12345", "p7zip-full.deb",
+                 "/home/user2/p7zip-full.deb")
+    res.append(ssh_checkout("0.0.0.0", "user2", "12345", "echo '12345' | sudo -S dpkg -i /home/user2/p7zip-full.deb", "Настраивается пакет"))
+    res.append(ssh_checkout("0.0.0.0", "user2", "12345", "echo '12345' | sudo -S dpkg -s p7zip-full", "Status: install ok installed"))
+    return res
+
+print(deploy())
--- Seminar_3/test_pos.py
+++ Seminar_3/test_pos.py
@@ -1,32 +1,59 @@
-from checkout import checkout
+from checkout import *
+from sshcheckers import *
 import yaml
 
-with open('config.yaml') as f:
+with open('config.yml') as f:
     data = yaml.safe_load(f)
 FOLDER_TST = data["FOLDER_TST"]
 FOLDER_OUT = data["FOLDER_OUT"]
 FOLDER_folder1 = data["FOLDER_folder1"]
 
 
-def test_step1(clear_dir, get_dir, make_file):
+def safe_log(start_time, name):
+    with open(name, "w", encoding="utf-8") as f:
+        f.write(''.join(get_out(f"journalctl --since '{start_time}'")[0]))
+
+
+def test_1(start_time):
+    res = []
+    upload_files("0.0.0.0", "user2", "12345", "p7zip-full.deb",
+                 "/home/user2/p7zip-full.deb")
+    res.append(ssh_checkout("0.0.0.0", "user2", "12345", "echo '12345' | sudo -S dpkg -i /home/user2/p7zip-full.deb",
+                            "Настраивается пакет"))
+    res.append(ssh_checkout("0.0.0.0", "user2", "12345", "echo '12345' | sudo -S dpkg -s p7zip-full",
+                            "Status: install ok installed"))
+    safe_log(start_time, "log_test1")
+    assert all(res), "test1 FAIL"
+
+
+def test_step1(clear_dir, get_dir, make_files, start_time):
     res1 = checkout(f"cd {FOLDER_TST}; 7z a {FOLDER_OUT}/arx2", "Everything is Ok")
     res2 = checkout(f"ls {FOLDER_OUT}", "arx2.7z")
+    safe_log(start_time, "log_test2")
     assert res1 and res2, "test1 FAIL"
 
 
-def test_step2():
-    res1 = checkout(f"cd {FOLDER_OUT}; 7z e arx2.7z -o{FOLDER_folder1} -y", "Everything is Ok")
-    res2 = checkout(f"ls {FOLDER_folder1}", "test1.txt")
-    assert res1 and res2, "test2 FAIL"
+def test_step2(clear_dir, get_dir, make_files, start_time):
+    res = []
+    res.append(checkout(f"cd {FOLDER_TST}; 7z a {FOLDER_OUT}/arx2", "Everything is Ok"))
+    res.append(checkout(f"cd {FOLDER_OUT}; 7z e arx2.7z -o{FOLDER_folder1} -y", "Everything is Ok"))
+    for i in make_files:
+        res.append(checkout(f"ls {FOLDER_folder1}", i))
+    safe_log(start_time, "log_test3")
+    assert all(res), "test2 FAIL"
 
 
-def test_step3():
+def test_step3(start_time):
+    safe_log(start_time, "log_test4")
     assert checkout(f"cd {FOLDER_OUT}; 7z t arx2.7z", "Everything is Ok"), "test3 FAIL"
 
 
-def test_step4():
-    assert checkout(f"cd {FOLDER_OUT}; 7z d arx2.7z", "Everything is Ok"), "test4 FAIL"
+def test_step4(get_list, start_time):
+    safe_log(start_time, "log_test5")
+    assert checkout(f"cd {FOLDER_OUT}; 7z d arx2.7z {get_list[0]}", "Everything is Ok"), "test4 FAIL"
 
 
-def test_step5():
-    assert checkout(f"cd {FOLDER_OUT}; 7z u arx2.7z", "Everything is Ok"), "test5 FAIL"
+def test_step5(get_list, start_time):
+    safe_log(start_time, "log_test6")
+    assert checkout(f"cd {FOLDER_TST}; echo 'hello' >> {get_list[1]}; cd {FOLDER_OUT}; 7z u arx2.7z "
+                    f"{FOLDER_TST}/{get_list[1]}", "Everything is Ok"), "test5 FAIL"
